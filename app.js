9c37c49111e5dfcb537d4cd07d464ed74fdb0f8c
Nar <abylay.kurakbayev@gmail.com>
2023-01-20 17:51:21 +0600

    feat: done statics

--- app.js
+++ app.js
@@ -1,39 +1,3 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 /**
 let info: {
     officeId: number;
@@ -600,34 +564,6 @@ class Admin {
 const admin = new Admin();
 admin.role = 1;
 
-//  **************** Конструктор
-
-class User {
-    name!: string;
-    age!: number;
-    constructor();
-    constructor(name: string);
-    constructor(age: number);
-    constructor(name: string, age: number,);
-    constructor(ageOrName?: string | number, age?: number) {
-        if (typeof ageOrName === 'string') {
-            this.name = ageOrName;
-        } else if (typeof ageOrName === 'number') {
-            this.age = ageOrName;
-        }
-        if (typeof age === 'number') {
-            this.age = age;
-        }
-    }
-}
-
-const user = new User('Nar');
-const user2 = new User();
-const user3 = new User(33);
-const user4 = new User('mir', 33);
-
-
-
 //  **************** Methods
 
 enum PaymentStatus {
@@ -693,32 +629,248 @@ function run(distance: number | string): number | string {
 
 run()
 
-*/
+
 //  ************* Getter and Setter
-var User = /** @class */ (function () {
-    function User() {
+class User {
+    _login!: string;
+    _password!: string;
+    _createdAt!: Date;
+
+    set login(l: string | number) {
+        this._login = 'user-' + l;
+        this._createdAt = new Date();
     }
-    Object.defineProperty(User.prototype, "login", {
-        get: function () {
-            return this._login;
-        },
-        set: function (l) {
-            this._login = 'user-' + l;
-            this._createdAt = new Date();
-        },
-        enumerable: false,
-        configurable: true
-    });
-    User.prototype.setPassword = function () {
-        return __awaiter(this, void 0, void 0, function () {
-            return __generator(this, function (_a) {
-                return [2 /*return*/];
-            });
-        });
-    };
-    return User;
-}());
-var user = new User();
+    get login() {
+        return this._login;
+    }
+    async setPassword(p: string) {
+
+    }
+}
+const user = new User();
 user.login = 'myLogin';
 console.log(user);
 console.log(user.login);
+
+//  **************** Конструктор
+
+class User {
+    name!: string;
+    age!: number;
+
+    constructor();
+    constructor(name: string);
+    constructor(age: number);
+    constructor(name: string, age: number);
+    constructor(ageOrName?: string | number, age?: number) {
+        if (typeof ageOrName === 'string') {
+            this.name = ageOrName;
+        } else if (typeof ageOrName === 'number') {
+            this.age = ageOrName;
+        }
+        if (typeof age === 'number') {
+            this.age = age;
+        }
+    }
+}
+
+const user = new User('nar');
+const user2 = new User();
+const user3 = new User(33);
+const user4 = new User('Nar', 33);
+
+//  ************* Implements
+interface ILogger {
+    log(...args): void;
+    error(...args): void;
+}
+class Logger implements ILogger {
+    log(...args: any[]): void {
+        console.log(...args);
+    }
+    async error(...args: any[]): Promise<void> {
+        // Кинуть во внешнюю систему
+        console.log(...args);
+    }
+}
+interface IPayable {
+    pay(paymentId: number): void;
+    price?: number;
+}
+
+interface IDeletable {
+    delete(): void
+}
+class User implements IPayable, IDeletable {
+    delete(): void {
+        throw new Error("Method not implemented.");
+    }
+    pay(paymentId: number | string): void {
+        ///
+    }
+    price?: number | undefined;
+}
+
+// *************** Extends Наследование
+
+type PaymentStatus = 'new' | 'paid'
+class Payment {
+    id!: number;
+    status: PaymentStatus = 'new';
+
+    constructor(id: number) {
+        this.id = id;
+    }
+
+    pay() {
+        this.status = 'paid'
+    }
+}
+class PersistedPayment extends Payment {
+    databaseId!: number;
+    paidAt!: Date
+
+    constructor() {
+        const id = Math.random();
+        super(id)
+    }
+
+    save() {
+        //
+    }
+    override pay(date?: Date) {
+        super.pay();
+        if (date) {
+            this.paidAt = date;
+        }
+    }
+}
+
+new PersistedPayment();
+class User {
+    name: string = 'user';
+
+    constructor() {
+        console.log(this.name);
+
+    }
+}
+
+class Admin extends User {
+    name: string = 'admin';
+    constructor() {
+        super();
+        console.log(this.name);
+    }
+}
+new Admin();
+// new Error('');
+class HttpError extends Error {
+    code!: number;
+    constructor(message: string, code?: number) {
+        super(message);
+        this.code = code ?? 500;
+    }
+}
+
+// **************** Композиция против наследования
+
+class User {
+    name!: string;
+
+    constructor(name: string) {
+        this.name = name
+    }
+
+}
+
+class Users extends Array<User> {
+    searchByName(name: string) {
+        return this.filter(u => u.name === name)
+    }
+    override toString(): string {
+        return this.map(u => u.name).join(', ')
+    }
+}
+
+const users = new Users();
+users.push(new User('Nar'));
+users.push(new User('Mir'));
+console.log(users.toString());
+
+class UserList {
+    users!: User[];
+
+    push(u: User) {
+        this.users.push(u);
+    }
+}
+
+class Payment {
+    date!: Date;
+
+}
+
+class UserWithPayment extends Payment {
+    name!: string;
+}
+
+class UserWithPayment2 {
+    user!: User;
+    paymant!: Payment;
+
+    constructor(user: User, payment: Payment) {
+        this.paymant = payment;
+        this.user = user;
+    }
+}
+
+*/
+// ************* Видимость свойств
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Vehicle = /** @class */ (function () {
+    function Vehicle() {
+    }
+    // #price: number;
+    // set model(m: string) {
+    //     this._model = m;
+    //     // this.#price = 100;
+    // }
+    // get model() {
+    //     return this._model;
+    // }
+    Vehicle.prototype.isPriceEqual = function (v) {
+        // return this.#price === v.#price;
+    };
+    Vehicle.prototype.addDamage = function (damage) {
+        this.damages.push(damage);
+    };
+    return Vehicle;
+}());
+var EuroTrack = /** @class */ (function (_super) {
+    __extends(EuroTrack, _super);
+    function EuroTrack() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    EuroTrack.prototype.setRun = function (km) {
+        this.run = km / 0.62;
+        // 
+    };
+    return EuroTrack;
+}(Vehicle));
+new Vehicle();
+new EuroTrack();
---
+++ test.ts
@@ -0,0 +1,61 @@
+class Product {
+    constructor(
+        public id: number,
+        public title: string,
+        public price: number
+    ) { }
+}
+
+class Delivery {
+    constructor(public date: Date) { }
+}
+
+class HomeDelivery extends Delivery {
+    constructor(date: Date, public address: string) {
+        super(date)
+    }
+}
+class ShopDelivery extends Delivery {
+    constructor(date: Date, public shopId: string) {
+        super(new Date())
+    }
+}
+type DeliveryOptions = HomeDelivery | ShopDelivery;
+class Cart {
+    private products: Product[] = [];
+    private delivery!: DeliveryOptions;
+
+    public addProduct(product: Product): void {
+        this.products.push(product);
+    }
+    public deleteProductById(productId: number): void {
+        this.products = this.products.filter((p: Product) => p.id !== productId)
+    }
+    public getSum(): number {
+        return this.products
+            .map((p: Product) => p.price)
+            .reduce((p1: number, p2: number) => p1 + p2)
+    }
+
+    public setDelivery(delivery: DeliveryOptions): void {
+        this.delivery = delivery;
+    }
+    public checkOut() {
+        if (this.products.length === 0) {
+            throw new Error('there is no products in the Cart!')
+        }
+        if (!this.delivery) {
+            throw new Error('there is no Delivery Option!')
+        }
+        return { success: true }
+    }
+}
+
+const cart = new Cart();
+cart.addProduct(new Product(1, 'choco', 100));
+cart.addProduct(new Product(2, 'coke', 120));
+cart.addProduct(new Product(2, 'beer', 150));
+cart.deleteProductById(1);
+cart.setDelivery(new HomeDelivery(new Date(), 'Almaty'));
+console.log(cart.getSum());
+console.log(cart.checkOut());
